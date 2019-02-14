23f95f8691dd1ec240fd1298dc1b4f715016acef
ryanscottjohnson <teacherman1@gmail.com>
2019-02-14 10:56:13 -0800

    removed util file from lib

--- lib/utility.jsx
+++
@@ -1,8 +0,0 @@
-export const renderWhen = (test, component, componentTwo) => 
-  test ? component : componentTwo || undefined;
-
-  export const toggler = options => 
-    object
-      .keys(options)
-      .filter(keys => !!options[keys])
-      .join(' ')
