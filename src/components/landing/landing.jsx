e5dcd47ba374c29a2fb47c6833bad7d098133e5c
ryanscottjohnson <teacherman1@gmail.com>
2019-02-14 11:04:10 -0800

    cleaning up landing page

--- src/components/landing/landing.jsx
+++ src/components/landing/landing.jsx
@@ -1,7 +1,4 @@
 import React from 'react';
-// import Header from '../header/header.jsx';
-// import Footer from '../footer/footer.jsx';
-// import Location from '../site-locaiton/location.jsx';
 
 import './landing.scss';
 
@@ -12,22 +9,17 @@ class Landing extends React.Component {
 
   render() {
     return (
-      <div className="landing-page">
-        test landing page
-      </div>
-      // <section id="landing-page">
-      //   <div className="greeting">
+      // <div className="landing-page">
+      //   test landing page
+      // </div>
+      <section id="landing-page">
+        <div className="greeting">
 
-      //     <p>Welcome Back!</p>
+          <p>Welcome Back!</p>
 
-      //   </div>
-      //   <div className="landing">
-      //     <p>Content</p>
-      //   </div>
+        </div>
+      </section>
 
-
-      // </section>
- 
     )
   }
 }
