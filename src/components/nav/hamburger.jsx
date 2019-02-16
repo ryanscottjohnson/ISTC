abf35b387f6605d9337deaf61968b94e01c4f558
ryanscottjohnson <teacherman1@gmail.com>
2019-02-15 22:35:34 -0800

    hamburger stuff

--- src/components/nav/hamburger.jsx
+++ src/components/nav/hamburger.jsx
@@ -2,20 +2,25 @@ import React from 'react';
 import './nav.scss';
 
 class Hamburger extends React.Component {
-  constructor(props){
+	constructor(props) {
 		super(props);
 	}
-render () {
-	return (
-		<div onClick={this.props.handleMenuOpenClose} className="wrapper">
-			<div className={this.props.openMenu ? 'show-nav active' : 'show-nav'}>
-				<span />
-				<span />
-				<span />
+	render() {
+		return (
+			<div onClick={this.props.handleMenuOpenClose} className="wrapper">
+				<div className={this.props.openMenu ? 'show-nav active' : 'show-nav'}>
+					<div className="menu">
+						<span className="menu-global menu-top"></span>
+						<span className="menu-global menu-middle"></span>
+						<span className="menu-global menu-bottom"></span>
+					</div>
+					{/* <span />
+					<span />
+					<span /> */}
+				</div>
 			</div>
-		</div>
-	)
-}
+		)
+	}
 }
 
 export default Hamburger;
