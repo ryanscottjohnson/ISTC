6af8684f65d04dc4b6083c7e9b31a2af3860fc08
ryanscottjohnson <teacherman1@gmail.com>
2019-02-16 21:53:31 -0800

    polish site location bar

--- src/components/header/BreadCrumb.jsx
+++ src/components/header/BreadCrumb.jsx
@@ -10,7 +10,7 @@ class BreadCrumb extends React.Component {
 
   renderBreadCrumb() {
     const { level1, level2, level3 } = this.props;
-    const breadcrumber = `${level1 != null ? level1 : 'Site Landing Page'} ${level2 != null ? ' > ' + level2 : ''} ${
+    const breadcrumber = `${level1 != null ? level1 : 'Section > Page Title'} ${level2 != null ? ' > ' + level2 : ''} ${
       level3 != null ? ' > ' + level3 : ''
     }`;
     return breadcrumber;
